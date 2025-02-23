ea92b331de715d951c5ec24967bbc124fcd3d7d8
Saad Baig <saadbaig200@gmail.com>
2025-02-23 14:59:59 +0000

    added helmet in server for protection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.use(helmet());
   app.enableCors({
     origin: configService.get('FE_URL'),
     methods: configService.get('ALLOWED_METHODS'),
