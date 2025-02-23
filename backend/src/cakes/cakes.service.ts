67ab5c905624d7b1c428f3184c6219f4ef3cb3c8
Saad Baig <saadbaig200@gmail.com>
2025-02-23 18:18:52 +0000

    fixed update issue

--- backend/src/cakes/cakes.service.ts
+++ backend/src/cakes/cakes.service.ts
@@ -1,9 +1,14 @@
-import { BadRequestException, HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cake } from './schemas/cake.schema';
 import { CreateCakeDto } from './dto/create-cake.dto';
 import { UpdateCakeDto } from './dto/update-cake.dto';
+import { AppConstants } from '@core/constants/appContants';
 
 @Injectable()
 export class CakesService {
@@ -15,19 +20,20 @@ export class CakesService {
 
   async findOne(id: string): Promise<Cake> {
     const cake = await this.cakeModel.findById(id).exec();
-    if (!cake) throw new NotFoundException('Cake not found');
+    if (!cake) throw new NotFoundException(AppConstants.MESSGES.CAKE_NOT_FOUND);
     return cake;
   }
 
   async create(createCakeDto: CreateCakeDto): Promise<Cake> {
     if (await this.cakeModel.findOne({ name: createCakeDto.name })) {
-      throw new BadRequestException('Cake already exists');
+      throw new BadRequestException(AppConstants.MESSGES.CAKE_EXISTS);
     }
     const createdCake = new this.cakeModel(createCakeDto);
     return createdCake.save();
   }
 
   async update(id: string, updateCakeDto: UpdateCakeDto): Promise<Cake> {
+    await this.findOne(id);
     return this.cakeModel.findByIdAndUpdate(id, updateCakeDto, { new: true });
   }
 
--- backend/src/core/constants/appContants.ts
+++ backend/src/core/constants/appContants.ts
@@ -3,5 +3,7 @@ export const AppConstants = Object.freeze({
     SUCCESSFUL_REQUEST: 'Request was successful',
     UNSUCCESSFUL_REQUEST: 'Request failed',
     SERVER_ERROR: 'Internal server error',
+    CAKE_NOT_FOUND: 'Cake not found',
+    CAKE_EXISTS: 'Cake already exists',
   },
 });
